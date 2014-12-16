e46a2506e665f6b1463b87dd87123ee1406dd4ff
Nathanael Gordon <bydand92nd@yahoo.com.au>
2014-12-16 15:51:33 +1100

    Adjusted printMenu method for use in the menu class

--- source/menu.py
+++ source/menu.py
@@ -3,20 +3,24 @@
 
 Class Menu:
     
-    def printMenu(self, strings = None, exitString = "Return to previous menu"):
+    def __init__(self, exitString = "Return to previous menu"):
+        self.options = []
+        self.exitString = exitString
+    
+    def printMenu(self, values):
         validResult = False
         
         while not validResult:
             count = 0
             result = 0
-            for string in strings:
+            for tup, func in self.options:
                 if count == 0:
-                    print("{0}\n".format(string))
+                    print("{0}\n".format(tup))
                 else:
-                    print("{0} - {1}".format(count, string))
+                    print("{0} - {1}".format(count, tup))
                 count += 1
                 
-            print("0 - {0}\n".format(exitString))
+            print("0 - {0}\n".format(self.exitString))
             
             try:
                 result = int(input("Selection: "))
@@ -27,4 +31,9 @@ Class Menu:
             except ValueError:
                 print("\nPlease enter a number in the range of 0 - {0}\n".format(count-1))
             
-        return result
+        return self.options[1](values)
+    
+    def addOption(self, string, func):
+        self.options.append((string, func))
+    
+    
